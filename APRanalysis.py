5a93e41a0b62681b3127371420a9193d5953e354
Vespertili0 <97523486+Vespertili0@users.noreply.github.com>
2022-06-29 11:41:23 +1000

    Create APRanalysis.py

---
+++ APRanalysis.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import pandas as pd
+import parmed as pmd
+import paprika.analysis as analysis
+from paprika.io import load_restraints
+from paprika.restraints.utils import extract_guest_restraints
+
